2ed7edd78fa22bcd9af048dd604d187b6cc70cc4
Francis Bond <francisbond1996@gmail.com>
2022-01-05 09:47:19 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// Pseudo coding
+// Make a start screen with a start button
+// Event listener to start program
+    //When start button pressed
+        // make an array of questions and answers 
+        // show random question and answers
+            // check to see if correct or not
+            // if correct
+                // add a point to score counter
+                //show next question
+            // if false
+                // subtract 5s from current time left
+                //show next question
+        // start a timer
+            // when timer ends, end game
+            // show score
+            // give option to add intials
+            // add highscore to a table showing score and intials
+            // make highscore save to local storage
+            // give option to play again
+            // make clear button, erase local storage
